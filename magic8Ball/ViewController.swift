556b060b7c786949fa1f1a87045a4848bb80c8ee
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-21 20:48:21 +0300

    Initial Commit

---
+++ magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic8Ball
+//
+//  Created by Kağan Değirmen on 21.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
