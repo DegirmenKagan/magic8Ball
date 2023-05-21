c747c2724254edebfe2bf87f34f0e2e970455188
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-21 23:09:51 +0300

    Added button and askButtonPressed functionality

--- magic8Ball/ViewController.swift
+++ magic8Ball/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var imageView: UIImageView!
+    let ballImages = [
+        UIImage(named: "ball1"),
+        UIImage(named: "ball2"),
+        UIImage(named: "ball3"),
+        UIImage(named: "ball4"),
+        UIImage(named: "ball5"),
+    ]
 
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballImages.randomElement()!
+    }
 
 }
 
